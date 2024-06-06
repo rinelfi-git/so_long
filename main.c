3040083e7d059904dd966d3c83bb30e4360295f9
Rijaniaina Elie Fidèle <elierijaniaina@gmail.com>
2024-06-06 22:28:17 +0300

    add map and change exit image

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: erijania <erijania@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 16:42:12 by erijania          #+#    #+#             */
-/*   Updated: 2024/06/06 21:26:55 by erijania         ###   ########.fr       */
+/*   Updated: 2024/06/06 22:15:07 by erijania         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,9 @@ int	handle_key(int keysim, t_frame *frame)
 		return (frame_close(frame));
 	map = (t_map *)node_get(frame->components, 0);
 	sprites = (t_sprites *)node_get(frame->components, 1);
+	player_locate(map, locations);
+	if (locations[0] == -1 && locations[1] == -1)
+		return (frame_close(frame));
 	if (keysim == XK_z)
 		player_up(sprites, map);
 	else if (keysim == XK_d)
@@ -36,21 +39,20 @@ int	handle_key(int keysim, t_frame *frame)
 		player_down(sprites, map);
 	else if (keysim == XK_q)
 		player_left(sprites, map);
-	player_locate(map, locations);
-	if (locations[0] == -1 && locations[1] == -1)
-		return (frame_close(frame));
 	frame_repaint(frame, map, sprites);
 	return (0);
 }
 
-int	main(void)
+int	main(int argc, char **argv)
 {
 	t_frame		*frame;
 	t_map		*map;
 	t_sprites	*sprites;
 
+	if (argc != 2)
+		return (1);
 	frame = frame_create();
-	map = map_new(open("./assets.d/map.d/correct.ber", O_RDONLY));
+	map = map_new(open(argv[1], O_RDONLY));
 	sprites = sprites_init(frame->x);
 	vec_add(frame->components, (t_node *) map);
 	vec_add(frame->components, (t_node *) sprites);
